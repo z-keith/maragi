4535e14273c679532a7e9544086f8cf208f351f7
Zack Keith <zkeith.inbox@gmail.com>
2017-11-19 22:23:43 -0500

    API routes defined

--- instance/app.py
+++ instance/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 # local import
 from instance.config import app_config
 from instance.db import db, init_testdb
+from api.endpoints import api_bp
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,6 +15,8 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    app.register_blueprint(api_bp)
 
     with app.app_context():
     	db.drop_all()
